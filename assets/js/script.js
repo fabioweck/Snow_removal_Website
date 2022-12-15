55f5778aa5ee641467230d714210fb5aff51dd77
Fabio Weck <fabio.weck@gmail.com>
2022-12-15 11:45:55 -0700

    New page (about) and hover links

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,13 +27,13 @@ var message = document.getElementById('message')
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("other-services").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+  var myDropdown = document.getElementById("other-services");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
