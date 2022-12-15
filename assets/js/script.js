77bd5878e4c8fd4291dd187d28595d888566ab57
Fabio Weck <fabio.weck@gmail.com>
2022-12-15 14:41:07 -0700

    navbar updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,13 +21,13 @@ var message = document.getElementById('message')
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("drop-ots-content").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+  if (!e.target.matches('.ots-button')) {
+  var myDropdown = document.getElementById("drop-ots-content");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
